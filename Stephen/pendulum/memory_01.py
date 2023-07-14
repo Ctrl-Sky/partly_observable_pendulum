96b73a08bc2b55975122d2f8d8300069c26d7eeb
Stephen Kelly <kellys32@mcmaster.ca>
2023-07-13 20:40:26 -0400

    exp

--- Stephen/pendulum/memory_01.py
+++ Stephen/pendulum/memory_01.py
@@ -480,7 +480,7 @@ def evalIndividual(individual, test=False):
         observation = observation[0]
         episode_reward = 0
         num_steps = 0
-        max_steps = 400
+        max_steps = 300
         timeout = False
         memory = [0.0, 0.0]
         while not (done or timeout):
