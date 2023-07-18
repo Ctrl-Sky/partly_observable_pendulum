6376ceafad66481114b9c906ec99438045fe1a16
kellys32 <kellys32@mcmaster.ca>
2023-07-18 12:00:59 -0400

    exp

--- Stephen/pendulum/ccanada.sh
+++ Stephen/pendulum/ccanada.sh
@@ -14,6 +14,10 @@ do
    esac
 done
 
-module load python
-source /home/skelly/deap_env/bin/activate
+module load python/3.10
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r ../requirements_alliancecan.txt
+
 python -u memory_01.py $seed > memory_01-$seed-$SLURM_JOB_ID.std
