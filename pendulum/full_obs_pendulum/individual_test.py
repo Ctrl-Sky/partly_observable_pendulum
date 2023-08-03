db36bcc323fea1c26b02b3f18c1711c05af286c1
Sky <quans6@mcmaster.ca>
2023-08-03 12:57:22 -0400

    remove pandas from grav test

--- pendulum/full_obs_pendulum/individual_test.py
+++ pendulum/full_obs_pendulum/individual_test.py
@@ -8,12 +8,12 @@ from deap import gp
 # Import modules from different directory
 import os
 import sys
-PATH=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(PATH)
+path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path)
 
 from modules.prim_functions import *
 from modules.output_functions import *
-from modules.test_gravity import fullObsTestGravity
+from modules.eval_individual import fullObsEvalIndividual
 
 # Set up primitives and terminals
 pset = gp.PrimitiveSet("MAIN", 3)
@@ -24,26 +24,22 @@ pset.renameArguments(ARG0='x')
 pset.renameArguments(ARG1='y')
 pset.renameArguments(ARG2='vel')
 
-<<<<<<< HEAD
 # Test individual at different gravities and takes the average fitness
 # of 5 and writes it to an excel sheet
-def fullObsTestGravity(ind, pset, sheet, path):
-    gravity = ['ind', 1, 2, 3, 4, 5, 6, 7, 8, 9.81, 11, 12, 13, 14, 15, 16, 17]
-    add_to_excel = [ind]
-    for i in range(1, len(gravity)):
+def fullObsTestGravity(ind, pset, path):
+    gravity = [1, 2, 3, 4, 5, 6, 7, 8, 9.81, 11, 12, 13, 14, 15, 16, 17]
+    for i in gravity:
+        add_to_excel = []
         total = 0
+        add_to_excel.append(i)
 
-        for j in range(3):
-            fit = fullObsEvalIndividual(ind, pset, gravity[i], test=False)[0]
+        for j in range(5):
+            fit = fullObsEvalIndividual(ind, pset, i, test=False)[0]
             total += fit
-        
+
         add_to_excel.append(round(total/5, 2))
+        write_to_excel(add_to_excel, path)
 
-    df = pd.DataFrame([add_to_excel], index=[1], columns=gravity)
-    write_to_excel(path, sheet, df_to_append=df)
-
-=======
->>>>>>> parent of aee905b (move test gravity functions back)
 # Replace value of str to an individuals tree in string form to test it
 # Can simply print the indivudual to output the ind's tree in string form
 # in string form and just copy and paste it here
@@ -61,4 +57,4 @@ ind=gp.PrimitiveTree.from_string(str, pset)
 
 # Test the ind at different gravity values and then
 # writes the fitness score at each gravity to full_obs_grav.xlsx
-fullObsTestGravity(ind, pset, sheet='9.81', path=os.path.dirname(os.path.abspath(__file__)) + "/full_obs_grav.xlsx")
+fullObsTestGravity(ind, pset, path=os.path.dirname(os.path.abspath(__file__)) + "/full_obs_grav.xlsx")
--- pendulum/part_obs_pendulum/individual_test.py
+++ pendulum/part_obs_pendulum/individual_test.py
@@ -4,18 +4,17 @@
 import math
 import operator
 
-import pandas as pd
 from deap import gp
 
 # Import modules from different directory
 import os
 import sys
-PATH=os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Saves the path to pendulum directory to PATH
-sys.path.append(PATH)
+path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path)
 
 from modules.prim_functions import *
 from modules.output_functions import *
-from modules.test_gravity import partObsTestGravity
+from modules.eval_individual import partObsEvalIndividual
 
 # Set up primitives and terminals
 pset = gp.PrimitiveSet("MAIN", 6)
@@ -40,42 +39,38 @@ pset.renameArguments(ARG3='x2')
 pset.renameArguments(ARG4='y3')
 pset.renameArguments(ARG5='x3')
 
-<<<<<<< HEAD
-def partObsTestGravity(ind, pset, sheet, path):
-    gravity = ['ind', 1, 2, 3, 4, 5, 6, 7, 8, 9.81, 11, 12, 13, 14, 15, 16, 17]
+def partObsTestGravity(ind, pset, path):
+    gravity = [1, 2, 3, 4, 5, 6, 7, 8, 9.81, 11, 12, 13, 14, 15, 16, 17]
     add_to_excel = [ind]
-    for i in range(1, len(gravity)):
+    for i in gravity:
         total = 0
 
-        for j in range(3):
+        for j in range(5):
             fit = partObsEvalIndividual(ind, pset, gravity[i], test=False)[0]
             total += fit
-
         
         add_to_excel.append(round(total/5, 2))
+    write_to_excel(path, add_to_excel)
 
-    df = pd.DataFrame([add_to_excel], index=[1], columns=gravity)
-    write_to_excel(path, sheet, df_to_append=df)
-
-=======
->>>>>>> parent of aee905b (move test gravity functions back)
 # Replace value of str to an individuals tree in string form to test it
 # Can simply print the indivudual to output the ind's tree in string form
 # in string form and just copy and paste it here
+l = ['ang_vel(y1, y2, x1, protectedDiv(ang_vel(add(max(y1, x1), sub(x2, x3)), x1, y3, x3), limit(y1, x3, limit(y1, x1, ang_vel(x3, x1, x3, x2)))))']
 
-str='add(ang_vel(max(y3, y2), y1, atan(conditional(y1, y3), y1), x2), y2)'
-ind=gp.PrimitiveTree.from_string(str, pset)
+for i in l:
+    str=i
+    ind=gp.PrimitiveTree.from_string(str, pset)
 
-# Creates an env and displays the individual being tested and
-# then prints out it's fitness score
-# print(partObsEvalIndividual(ind, pset, 17, True))
+    # Creates an env and displays the individual being tested and
+    # then prints out it's fitness score
+    # print(partObsEvalIndividual(ind, pset, 17, True))
 
-# Plots the graph of the ind in a more falttering way and
-# saves it to a png to view
-# nodes, edges, labels = gp.graph(s)
-# plot_as_tree(nodes, edges, labels, 12)
+    # Plots the graph of the ind in a more falttering way and
+    # saves it to a png to view
+    # nodes, edges, labels = gp.graph(s)
+    # plot_as_tree(nodes, edges, labels, 12)
 
-# Test the ind at different gravity values and then
-# writes the fitness score at each gravity to part_obs_grav.xlsx
-partObsTestGravity(ind, pset, sheet='9,81', path=os.path.dirname(os.path.abspath(__file__)) + "/excel_sheets/part_obs_grav.xlsx")
-# partObsTestGravity(ind, pset, '9.81', "Book1.xlsx")
+    # Test the ind at different gravity values and then
+    # writes the fitness score at each gravity to part_obs_grav.xlsx
+    partObsTestGravity(ind, pset, path=os.path.dirname(os.path.abspath(__file__)) + "/excel_sheets/part_obs_grav.xlsx")
+
