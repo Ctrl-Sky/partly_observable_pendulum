639dfb66e0ba352c2d5fb3f78fab7d5aaee276ca
Sky <quans6@mcmaster.ca>
2023-08-08 13:08:38 -0400

    Get pdf for best ind

--- pendulum/part_obs_pendulum/individual_test.py
+++ pendulum/part_obs_pendulum/individual_test.py
@@ -55,11 +55,7 @@ def partObsTestGravity(ind, pset, string, path):
 # Replace value of str to an individuals tree in string form to test it
 # Can simply print the indivudual to output the ind's tree in string form
 # in string form and just copy and paste it here
-l=['protectedDiv(protectedDiv(y3, max(x2, y2)), conditional(x1, sin(max(x2, protectedDiv(y3, cos(protectedDiv(x3, conditional(x1, sin(max(tan(max(sin(acos(x1, y2)), x3)), y3))))))))))',
-   'add(ang_vel(sub(y1, x1), asin(y2, y2), limit(conditional(y2, y1), add(x2, x2), tan(y1)), ang_vel(x2, max(x3, y3), y3, x3)), acos(atan(max(max(y1, y3), x1), tan(x3)), limit(x1, ang_vel(add(x3, limit(conditional(y2, y1), add(x2, x2), tan(y1))), max(y1, x2), ang_vel(y2, x3, y3, x2), max(y2, y2)), protectedDiv(ang_vel(y2, atan(max(conditional(add(x1, y1), ang_vel(y1, x3, x3, y2)), x1), x2), x3, y3), cos(x3)))))',
-   'tan(protectedDiv(sin(max(x1, x1)), limit(x3, x3, x1)))',
-   'sub(asin(y3, y3), ang_vel(y1, y3, sin(atan(ang_vel(atan(y1, y3), x2, ang_vel(add(protectedDiv(x1, y3), limit(x2, y2, x2)), x2, asin(x1, x3), cos(atan(cos(x2), sin(x1)))), cos(atan(cos(asin(atan(y3, y1), limit(x3, x1, y2))), sin(x1)))), y1)), sin(conditional(x2, x2))))',
-   'limit(tan(protectedDiv(add(y2, x3), x1)), ang_vel(y3, y2, max(acos(y1, x1), sub(x3, limit(y3, y2, x3))), x3), acos(protectedDiv(add(x1, x1), max(x3, ang_vel(x2, x2, x1, y1))), protectedDiv(acos(add(sin(x1), protectedDiv(x1, sin(asin(x1, y1)))), y2), x3)))']
+l=['add(ang_vel(limit(max(x1, y1), add(y1, y2), conditional(y3, atan(y2, y3))), x3, y3, x3), ang_vel(atan(limit(y2, x1, x2), atan(y2, y3)), x3, ang_vel(acos(x2, x2), x3, x3, asin(sub(y1, y2), add(y3, y1))), x3))']
 
 for i in l:
     string=i
@@ -71,11 +67,10 @@ for i in l:
 
     # Plots the graph of the ind in a more falttering way and
     # # saves it to a png to view
-    # nodes, edges, labels = gp.graph(ind)
-    # plot_as_tree(nodes, edges, labels, 12)
+    nodes, edges, labels = gp.graph(ind)
+    plot_as_tree(nodes, edges, labels, 13)
 
     # Test the ind at different gravity values and then
     # writes the fitness score at each gravity to part_obs_grav.xlsx
-    partObsTestGravity(ind, pset, string, path=os.path.dirname(os.path.abspath(__file__)) + "/excel_sheets/part_obs_grav.xlsx")
-
+    # partObsTestGravity(ind, pset, string, path=os.path.dirname(os.path.abspath(__file__)) + "/excel_sheets/part_obs_grav.xlsx")
 
